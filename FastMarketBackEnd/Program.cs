ed5788b1ce34a34fab32a1501a5df79dc58b2f7a
Montse <montse@devtzal.com>
2024-10-03 19:30:34 -0500

    Conexión a la BD

--- FastMarketBackEnd/Program.cs
+++ FastMarketBackEnd/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using FastMarketBackEnd.Context;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 27))
+    ));
 
 var app = builder.Build();
 
