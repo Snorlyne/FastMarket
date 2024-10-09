9f088e2484f90cb6c682cd81932704236aabd3f1
Lian Erick Aguirre Sierra <113861220+Snorlyne@users.noreply.github.com>
2024-10-08 19:14:27 -0500

    feat: servicio e productos
    Creación del servicio de productos, etiquetas y fotos

---
+++ Common/Utilities/Response.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Utilities
+{
+    public class Response<T>
+    {
+        public Response() { }
+
+        public Response(T data, string message = null)
+        {
+            IsSuccess = true;
+            Message = message;
+            Result = data;
+        }
+        public Response(string message)
+        {
+            IsSuccess = false;
+            Message = message;
+        }
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public T Result { get; set; }
+    }
+}
---
+++ Services/IServices/IFotosServices.cs
@@ -0,0 +1,17 @@
+﻿using Common.Utilities;
+using Domain.Dto;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.IServices
+{
+    public interface IFotosServices
+    {
+        Task<Response<List<Fotos>>> CrearFoto(List<FotosDto> request, int idProducto);
+        Task<Response<List<Fotos>>> EliminarFoto(List<FotosDto> request);
+    }
+}
