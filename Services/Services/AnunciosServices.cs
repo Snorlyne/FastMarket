06a5b8a561cf4e70d05093d790294c7d17578c10
Lian Erick Aguirre Sierra <113861220+Snorlyne@users.noreply.github.com>
2024-11-13 20:16:26 -0500

    fix: anuncios services

--- Services/Services/AnunciosServices.cs
+++ Services/Services/AnunciosServices.cs
@@ -192,6 +192,7 @@ namespace Services.Services
                     .Include(a => a.Productos)
                         .ThenInclude(p => p.ProductosEtiquetas)
                             .ThenInclude(pe => pe.Etiquetas)
+                                                .Where(x => x.Estado == "activo")
                     .Select(a => new AnunciosDto
                     {
                         Id = a.Id,
